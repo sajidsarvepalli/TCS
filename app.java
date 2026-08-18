4cb4d4f1576d9df7eb9aa8e8dda2d405ec7511c2
sajidsarvepalli <sajidshaikktm@gmail.com>
2026-08-18 13:44:41 +0530

    sample code added to java file

---
+++ app.java
@@ -0,0 +1,8 @@
+// Every Java application starts with a class definition
+public class HelloWorld {
+    // The main method is the execution entry point for the JVM
+    public static void main(String[] args) {
+        // System.out.println prints text to the console
+        System.out.println("Hello, World!");
+    }
+}
